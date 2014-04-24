5bde58482aec284e25c34673c8e663dc81be2a7a
mwjordan <Nevermind>
2014-04-24 00:08:26 -0400

    added name and weight methods, changed Boomer's diet

--- fido.rb
+++ fido.rb
@@ -3,12 +3,26 @@ class Dog
     @name = name
     @weight = weight
   end
-  def one_month_older(growth)
-    # weight increases by minimum 1, plus increase from growth rate
-    @weight += 1 + (@weight * growth)
-    # print weight on one line then...
-    # convert from float to integer to round down and then to string
-    print "#{@name} weighs #{@weight.to_i.to_s} pounds"
+  # allow name reference via method
+  def name 
+    @name
+  end
+  # allow weight reference via method
+  def weight 
+    @weight
+  end 
+  def one_month_older(growth, age)
+    # weight increases by minimum, plus increase from growth rate
+    @weight += 1.5 + (@weight * growth)
+    puts "It's now one month later..."
+    # use print for no line break
+    if age == 1 # singular 'month' 
+        print "#{@name} is #{age} month old"
+    else # plural, months
+        print "#{@name} is #{age} months old"
+    end
+    # convert from float to integer to round down
+    puts " and weighs #{@weight.to_i} pounds."
   end
   def speak(age)
     if age < 18
@@ -19,21 +33,19 @@ class Dog
   end
 end
 
-dog = Dog.new("Boomer", 4) # Boomer is born
-puts "Boomer is born and weighs 3 lbs."
+dog = Dog.new("Boomer", 2) # Boomer is born
+# reference name and weight via methods
+puts "#{dog.name} is born and weighs #{dog.weight} pounds."
 puts
-age = 0  # initial age
-growth = 2.75  # initial growth rate
+age = 0  # initial age in months
+growth = 2.25 # initial growth rate
 18.times do
     age += 1
+    # dog ages one month
+    dog.one_month_older(growth, age)
+    # reduce growth rate by half
     growth = growth/2
-    puts "It's now one month later..."
-    dog.one_month_older(growth)
-    if age == 1 # singular 'month' 
-        puts " and is #{age.to_s} month old."
-    else # plural, months
-        puts " and is #{age.to_s} months old."
-    end
+    # dog bark
     dog.speak(age)
     puts 
 end
